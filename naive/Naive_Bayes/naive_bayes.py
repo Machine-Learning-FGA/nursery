256057a705231dd9a217091b2cc53f42cc4f9f26
Anderson Silva <andersoonn.ams@gmail.com>
2018-04-16 11:51:47 -0300

    addying notebook for naive bayes classifier

--- naive/Naive_Bayes/naive_bayes.py
+++ naive/Naive_Bayes/naive_bayes.py
@@ -23,6 +23,16 @@ dataset = pd.read_csv('Social_Network_Ads.csv')
 X = dataset.iloc[:, [2, 3]].values
 y = dataset.iloc[:, 4].values
 
+
+dataset = pd.read_csv('nursery.data.csv')
+X = dataset.iloc[:, 0:8].values
+y = dataset.iloc[:, 9:9].values
+
+
+
+
+
+
 # Splitting the dataset into the Training set and Test set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
